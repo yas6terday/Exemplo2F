cc9d7cac16865c7eba2ecbb458013241c375b93c
rm86581 <yasmimferro06@gmail.com>
2023-04-10 12:31:09 -0300

    laço de repetição

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int contador = 0;
+        while (contador < 10) {
+        System.out.println("contador = " + contador);
+        contador = contador + 1;
+        }
+
     }
+
 }
